8e104881cffeb86cd52ce2de75e9058cd4e86972
Márcio Trovão <mc872684@gmail.com>
2021-07-18 19:54:42 -0300

    Código atualizado

--- Multiplicação_simples.kt
+++ Multiplicação_simples.kt
@@ -2,5 +2,4 @@ Multiplicação simples
 
 let valor1 = parseInt(gets());
 let valor2 = parseInt(gets());
-let total = (valor1,valor2) => valor1 * valor2; 
-console.log("PROD = " + total(valor1,valor2));
+console.log(`PROD = ${valor1 * valor2}`);
