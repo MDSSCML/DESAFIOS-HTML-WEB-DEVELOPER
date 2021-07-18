976f7ec76b6167b219dfa63a463b81f564041cb1
Márcio Trovão <mc872684@gmail.com>
2021-07-18 20:41:48 -0300

    Código atualizado

--- Folha_de_pagamento.kt
+++ Folha_de_pagamento.kt
@@ -3,7 +3,7 @@ Folha de pagamento
 let valor1 = parseInt(gets());
 let valor2 = parseInt(gets());
 let valor3 = parseFloat(gets());
-let salary = (valor2,valor3) => valor2 * valor3;
-console.log("NUMBER = " + valor1);
-console.log("SALARY = U$ " + salary(valor2,valor3).toFixed(2));
+console.log(`NUMBER = ${valor1} \n SALARY = U$  ${(valor2 * valor3).toFixed(2)}`);
+
+
 
