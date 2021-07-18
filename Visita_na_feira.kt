97a9d5bf06655792a0c553bc2739692270c97715
Márcio Trovão <mc872684@gmail.com>
2021-07-18 19:48:26 -0300

    Código atualizado

--- Visita_na_feira.kt
+++ Visita_na_feira.kt
@@ -3,6 +3,4 @@ Desafio : Visita a feira
 let line = gets().split(" ");
 let A = parseInt(line[0]);
 let B = parseInt(line[1]);
-let total = A + B  ; // Altere o valor da variável com o cálculo esperado
-
-console.log("X =  "  + total );
+console.log(`X = ${A + B}`);
