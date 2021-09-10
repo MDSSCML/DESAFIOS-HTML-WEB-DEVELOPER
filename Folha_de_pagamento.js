9f11dc36311ed2d0de6cddfad6b116bb5707662c
Márcio Trovão <mc872684@gmail.com>
2021-09-09 23:42:39 -0300

    código atualizado

--- Folha_de_pagamento.js
+++ Folha_de_pagamento.js
@@ -1,9 +1,7 @@
 Folha de pagamento
 
-let valor1 = +gets();
-let valor2 = +gets();
-let valor3 = +gets();
-console.log(`NUMBER = ${valor1} \n SALARY = U$  ${(valor2 * valor3).toFixed(2)}`);
+console.log(`NUMBER = ${+gets()} 
+SALARY = U$  ${(+gets() * +gets()).toFixed(2)}`);
 
 
 
