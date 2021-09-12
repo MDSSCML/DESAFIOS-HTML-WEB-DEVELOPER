0e841ce7c895f9e1cba8efd6b021bbc91b06015e
Márcio Trovão <mc872684@gmail.com>
2021-09-12 20:57:08 -0300

    atualizado

--- Visita_na_feira.js
+++ Visita_na_feira.js
@@ -1,4 +1,16 @@
 Desafio : Visita a feira
 
+//Código modificado
+
 const  [Linha,linha] = gets().split(" ");
-console.log(`X = ${+(Linha) + +(linha)}`);
+print(`X = ${+(Linha) + +(linha)}`);
+
+
+
+
+//Código original
+
+let line = gets().split(" ");
+let B = parseInt(line[1]);
+let A = parseInt(line[0]);
+console.log(`X = ${A + B}`);
